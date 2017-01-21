22dd37d0a6cbf525d45129903ba630ed381988b3
mwhite102 <mwhite102@gmail.com>
2017-01-21 13:50:23 -0600

    Impelemented Single Selection in the Matrix UserControl

--- EisenhowerMatrix.WPF/UserControls/Matrix.xaml.cs
+++ EisenhowerMatrix.WPF/UserControls/Matrix.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EisenhowerMatrix.WPF.Models;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EisenhowerMatrix.WPF.UserControls
 {
@@ -20,9 +9,63 @@ namespace EisenhowerMatrix.WPF.UserControls
     /// </summary>
     public partial class Matrix : UserControl
     {
+
+        /// <summary>
+        /// The selected MatrixTaskItemModel in the Matrix
+        /// </summary>
+        public MatrixTaskItemModel SelectedMatrixTaskItemModel
+        {
+            get { return (MatrixTaskItemModel)GetValue(SelectedMatrixTaskItemModelProperty); }
+            set { SetValue(SelectedMatrixTaskItemModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedMatrixTaskItemModelProperty =
+            DependencyProperty.Register("SelectedMatrixTaskItemModel", typeof(MatrixTaskItemModel), typeof(Matrix), new PropertyMetadata(null));
+
         public Matrix()
         {
             InitializeComponent();
+        }
+
+        private void MatrixBlock_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // If e.AddedItems is empty, then this is a SelectionChanged event from 
+            // a ListBox that had it's SelectedIndex set to -1. Just return
+            if (e.AddedItems.Count == 0)
+            {
+                return;
+            }
+
+            // All MatrixBlocks implement this event handler
+            // Clear any selected items all but the sender block
+            MatrixBlock matrixBlock = sender as MatrixBlock;
+            if (matrixBlock != null)
+            {
+                if (!sender.Equals(ImportantAndUrgentMatrixBlock))
+                {
+                    ImportantAndUrgentMatrixBlock.ClearSelection();
+                }
+
+                if (!sender.Equals(ImportantAndNotUrgentMatrixBlock))
+                {
+                    ImportantAndNotUrgentMatrixBlock.ClearSelection();
+                }
+
+                if (!sender.Equals(NotImportantAndUrgentMatrixBlock))
+                {
+                    NotImportantAndUrgentMatrixBlock.ClearSelection();
+                }
+
+                if (!sender.Equals(NotImportantAndNotUrgentMatrixBlock))
+                {
+                    NotImportantAndNotUrgentMatrixBlock.ClearSelection();
+                }
+            }
+
+            // Set the SelectedMatrixTaskItemModel value
+            SelectedMatrixTaskItemModel = e.AddedItems[0] as MatrixTaskItemModel;
+
+            e.Handled = true;
         }
     }
 }
--- EisenhowerMatrix.WPF/UserControls/MatrixBlock.xaml.cs
+++ EisenhowerMatrix.WPF/UserControls/MatrixBlock.xaml.cs
@@ -24,5 +24,51 @@ namespace EisenhowerMatrix.WPF.UserControls
         {
             InitializeComponent();
         }
+
+        #region Public Methods
+
+        /// <summary>
+        /// Clears the SelectedItem in the MatrixBlock
+        /// </summary>
+        public void ClearSelection()
+        {
+            MatrixBlock_ListBox.SelectedIndex = -1;
+        }
+
+        #endregion
+
+        #region SelectionChangedEvent
+
+        /// <summary>
+        /// Defines a new SelectionChangedEventHandler for the MatrixBlock
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public delegate void SelectionChangedEventHandler(object sender, SelectionChangedEventArgs e);
+
+        /// <summary>
+        /// Gets or sets the SelectionChanged event
+        /// </summary>
+        public event SelectionChangedEventHandler SelectionChanged;
+
+        /// <summary>
+        /// Raises the SelectedChanged event
+        /// </summary>
+        /// <param name="args">The SelectionChangedEventArgs</param>
+        private void RaiseSelectionChangedEvent(SelectionChangedEventArgs args)
+        {
+            if (this.SelectionChanged != null)
+            {
+                this.SelectionChanged(this, args);
+            }
+        }
+
+        #endregion
+
+        private void MatrixBlock_ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Raise the local SectionChanged event
+            RaiseSelectionChangedEvent(e);
+        }
     }
 }
